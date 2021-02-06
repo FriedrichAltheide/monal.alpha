ef728e7c23388b6da533482043f2269d4dfa8ede
Friedrich Altheide <11352905+FriedrichAltheide@users.noreply.github.com>
2021-02-06 09:55:58 +0100

    Test

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,15 +1,4 @@
 #!/bin/sh
-# build commandline used by travis:
-# set -o pipefail && xcodebuild -workspace Monal.xcworkspace -scheme Monal -destination platform\=iOS\ Simulator,OS\=13.2.2,name\=iPhone\ 8 build test | xcpretty
-
-if [[ "$TRAVIS_PULL_REQUEST" != "false" ]]; then
-  echo "This is a pull request. No deployment will be done."
-  exit 0
-fi
-if [[ "$TRAVIS_BRANCH" != "develop" ]]; then
-  echo "Testing on a branch other than develop. No deployment will be done."
-  exit 0
-fi
 
 # Abort on Error
 set -e
@@ -34,43 +23,39 @@ PING_LOOP_PID=$!
 
 ls -l ~/Library/MobileDevice/Provisioning\ Profiles/
 
+echo ""
+echo "*********************************"
+echo "*     Installing macOS Pods     *"
+echo "*********************************"
+pod install
+pod update
 
-if [ "$BUILD_MACOS" = true ] || grep -q "Friedrich" "$TRAVIS_BUILD_DIR/changes.txt"
-then
-	echo ""
-	echo "*********************************"
-	echo "*     Installing macOS Pods     *"
-	echo "*********************************"
-	pod install
-	pod update
-	
-	echo ""
-	echo "***************************"
-	echo "*     Archiving macOS     *"
-	echo "***************************"
-	xcrun xcodebuild -workspace "Monal.xcworkspace" -scheme "Monal" -sdk macosx -configuration Debug -destination 'generic/platform=macOS,variant=Mac Catalyst,name=Any Mac' -archivePath "build/macos_$APP_NAME.xcarchive" clean archive CODE_SIGN_IDENTITY="$APP_DEVELOPER_NAME" CODE_SIGN_STYLE="Manual" GCC_PREPROCESSOR_DEFINITIONS='$GCC_PREPROCESSOR_DEFINITIONS IS_ALPHA=1' BUILD_LIBRARIES_FOR_DISTRIBUTION=YES SUPPORTS_MACCATALYST=YES >> $BUILD_OUTPUT 2>&1
-	
-	echo ""
-	echo "****************************"
-	echo "*     Exporting macOS      *"
-	echo "****************************"
-	# see: https://gist.github.com/cocoaNib/502900f24846eb17bb29
-	# and: https://forums.developer.apple.com/thread/100065
-	# and: for developer-id distribution (distribution *outside* of appstore) an developer-id certificate must be used for building
-	xcodebuild -exportArchive -archivePath "build/macos_$APP_NAME.xcarchive" -exportPath "build/app" -exportOptionsPlist ../scripts/catalyst_exportOptions.plist
-	echo "build dir:"
-	ls -l "build"
-	
-	echo ""
-	echo "**************************"
-	echo "*     Packing macOS      *"
-	echo "**************************"
-	cd build/app
-	mv "$APP_NAME.app" "$APP_NAME.alpha.app"
-	tar -cf "$APP_NAME.tar" "$APP_NAME.alpha.app"
-	cd ../..
-	ls -l build/app
-fi
+echo ""
+echo "***************************"
+echo "*     Archiving macOS     *"
+echo "***************************"
+xcrun xcodebuild -workspace "Monal.xcworkspace" -scheme "Monal" -sdk macosx -configuration Debug -destination 'generic/platform=macOS,variant=Mac Catalyst,name=Any Mac' -archivePath "build/macos_$APP_NAME.xcarchive" clean archive CODE_SIGN_IDENTITY="$APP_DEVELOPER_NAME" CODE_SIGN_STYLE="Manual" GCC_PREPROCESSOR_DEFINITIONS='$GCC_PREPROCESSOR_DEFINITIONS IS_ALPHA=1' BUILD_LIBRARIES_FOR_DISTRIBUTION=YES SUPPORTS_MACCATALYST=YES >> $BUILD_OUTPUT 2>&1
+
+echo ""
+echo "****************************"
+echo "*     Exporting macOS      *"
+echo "****************************"
+# see: https://gist.github.com/cocoaNib/502900f24846eb17bb29
+# and: https://forums.developer.apple.com/thread/100065
+# and: for developer-id distribution (distribution *outside* of appstore) an developer-id certificate must be used for building
+xcodebuild -exportArchive -archivePath "build/macos_$APP_NAME.xcarchive" -exportPath "build/app" -exportOptionsPlist ../scripts/catalyst_exportOptions.plist
+echo "build dir:"
+ls -l "build"
+
+echo ""
+echo "**************************"
+echo "*     Packing macOS      *"
+echo "**************************"
+cd build/app
+mv "$APP_NAME.app" "$APP_NAME.alpha.app"
+tar -cf "$APP_NAME.tar" "$APP_NAME.alpha.app"
+cd ../..
+ls -l build/app
 	
 
 echo ""
